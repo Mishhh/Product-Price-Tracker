f88e861da7fd1af0f87fb60d21bab05e14ae5864
Mishalee Lambat <72599813+Mishhh@users.noreply.github.com>
2025-07-18 12:45:51 +0530

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -4,16 +4,16 @@ import pandas as pd
 from datetime import datetime
 import os
 
-# Scrape.do API setup
+
 API_TOKEN = "a9745136af2d4217a6b984674246d76b6602464fc32"
 product_url = "https://www.bigbasket.com/pd/10000027/fresho-banana-robusta-500-g/?nc=smart_basket"
 api_url = f"https://api.scrape.do?token={API_TOKEN}&url={product_url}"
 
-# Send request via Scrape.do
+
 response = requests.get(api_url)
 soup = BeautifulSoup(response.text, "html.parser")
 
-# Extract product name and price
+
 try:
     product_name = soup.find("h1", class_="Description___StyledH-sc-82a36a-2").get_text(strip=True)
     price_tag = soup.find("td", class_="Description___StyledTd-sc-82a36a-4")
@@ -25,7 +25,7 @@ try:
     print(f"✅ Product: {product_name}")
     print(f"💰 Price: {price}")
 
-    # Save to CSV
+    
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     row = {"Timestamp": now, "Product": product_name, "Price": price}
     filename = "banana_price_history.csv"
